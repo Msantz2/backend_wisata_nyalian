2a7683db6cd07d16a4e69cc5eee310be6a8c500c
Msantz2 <kadeksantike20@gmail.com>
2026-09-07 14:21:58 +0800

    koneksi database

--- src/config/database.js
+++ src/config/database.js
@@ -9,8 +9,14 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'postgres'
-  }
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
 );
 
 const pool = new Pool({
